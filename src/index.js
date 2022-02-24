5ccf59a10875773daac67f66aac67b40e5785a8e
Diana <diana.zhusupbekova.kk@gmail.com>
2022-02-25 02:04:44 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stringArray = str.split("");
+    let tempArray = [];
+    let result;
+    let i, flag;
+    
+    flag = 1;
+
+    for (i = 0; i < stringArray.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                stringArray[i] == bracketsConfig[j][0] &&
+                (bracketsConfig[j][0] != bracketsConfig[j][1] || flag == 1)
+            ) {
+                tempArray.push(stringArray[i]);
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) flag = 0;
+                continue;
+            } else if (stringArray[i] == bracketsConfig[j][1]) {
+                if (
+                    tempArray[tempArray.length - 1] != bracketsConfig[j][0] &&
+                    bracketsConfig[j][0] != bracketsConfig[j][1]
+                ) {
+                    return false;
+                }
+                if (tempArray[tempArray.length - 1] == bracketsConfig[j][0]) {
+                    tempArray.pop();
+                    if (bracketsConfig[j][0] == bracketsConfig[j][1]) flag = 1;
+                }
+            } else continue;
+        }
+    }
+
+    if (
+        typeof tempArray != "undefined" &&
+        tempArray != null &&
+        tempArray.length > 0
+    ) {
+        return false;
+    } else return true;
+};
